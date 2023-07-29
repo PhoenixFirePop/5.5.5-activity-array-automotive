7dd4511b032c09225cf9fcf8db45512f222c0656
Diana T <135925668+PhoenixFirePop@users.noreply.github.com>
2023-07-29 10:19:57 -0500

    testing instances

--- mercurySedan.js
+++ mercurySedan.js
@@ -36,16 +36,107 @@ class Car extends Vehicle {
         this.numberOfWheels = 0;
     }
 
-
-
-
-
 //TO DO: Creating Instances and Testing Them
 
-//You can use the same instance "v" of the Vehicle class above for the base class.
+    // start() {
+    //     if (this.fuel > 0) {
+    //         return this.started = true;
+    //         console.log("engine started...!!!");
+    //     } else {
+    //         return this.started = false;
+    //         console.log("engine cannot start...");
+    //     }
+    // }
+    // accelerate() {
+    //     if (this.started) {
+    //         if (this.fuel > 0) {
+    //             console.log(this.speed += 1);
+    //             this.fuel = this.fuel - 1;
+    //         } else {
+    //             console.log("out of fuel.");
+    //             this.stop();
+    //         }
+    //     } else {
+    //         alert("You need to start the engine first.");
+    //     }
+    // }
+    // decelerate() {
+    //     if (this.started) {
+    //         if (this.fuel > 0) {
+    //             if (this.speed > 0) {
+    //                 console.log(this.speed -= 1);
+    //                 this.fuel = this.fuel - 1;
+    //             } else {
+    //                 console.log(this + " has stopped moving");
+    //                 this.fuel = this.fuel - 1;
+    //             }
+    //         } else {
+    //             console.log("out of fuel.");
+    //             this.stop();
+    //         }
+    //     } else {
+    //         alert("You need to start the engine first.");
+    //     }
+    // }
+    // stop() {
+    //     this.started = false;
+    // }
+
+    // typeOfVehicle(wheels) {
+    //     if (this.numberOfWheels == 8 && 8 == wheels) {
+    //         console.log(this.model + " " + this.make + " is a Truck");
+    //     } else if (this.numberOfWheels == 4 && 4 == wheels) {
+    //         console.log(this.model + " " + this.make + " is a CAr");
+    //     } else if (this.numberOfWheels == 2 && 2 == wheels) {
+    //         console.log(this.model + " " + this.make + " is a Bike");
+    //     } else {
+    //         console.log("Unknown type of vehicle");
+    //     }
+    // }
+    // }
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
 
 
+}
 
 
+let myCar = new Car("Mercury", "Sedan", "1965", "color", "mileage")
 
-//Create at least two new instances of the Car class and test them here:
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
+
+
